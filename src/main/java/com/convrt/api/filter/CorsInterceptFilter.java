474d68898b3925b184d477d4cc07db17fe713841
Unknown <jeffreysapia@gmail.com>
2018-09-09 14:34:56 -0700

    Update to cors

--- src/main/java/com/convrt/api/filter/CorsInterceptFilter.java
+++ src/main/java/com/convrt/api/filter/CorsInterceptFilter.java
@@ -21,9 +21,7 @@ public class CorsInterceptFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		log.info("Value: {}", publicAddress);
-		// response.addHeader("Access-Control-Allow-Origin", "http://convrt-me-ui.s3-website-us-west-1.amazonaws.com");
-		response.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		response.addHeader("Access-Control-Allow-Origin", publicAddress);
 		response.addHeader("Access-Control-Allow-Headers", "Content-Type, Token");
 		response.addHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE,OPTIONS");
 		response.addHeader("Access-Control-Allow-Credentials", "true");
